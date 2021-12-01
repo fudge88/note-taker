2798674dc98a02070923af5a761e606868ac76a6
Fahra Akhlaq <f.akhlaq@outlook.com>
2021-12-01 11:57:28 +0000

    enpoint working in postman

--- src/controllers/api/notes.js
+++ src/controllers/api/notes.js
@@ -1,8 +1,34 @@
-const getNotes = (req, res) => {
-  res.send("getNotes");
+const fs = require("fs");
+const path = require("path");
+
+const getNotesFromFile = () => {
+  const notesJSON = fs.readFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    "utf-8"
+  );
+
+  const notes = JSON.parse(notesJSON);
+
+  return notes;
 };
+
+const getNotes = (req, res) => {
+  const notes = getNotesFromFile();
+  res.json(notes);
+};
+
 const getNoteById = (req, res) => {
-  res.send("getNoteById");
+  const { id } = req.params;
+  const notes = getNotesFromFile();
+  const note = notes.find((note) => {
+    return note.id === id;
+  });
+  if (note) {
+    return res.json(note);
+  }
+  return res.status(404).json({
+    message: `INVALID SEARCH - Note ID:${id} was not found`,
+  });
 };
 const addNote = (req, res) => {
   res.send("addNote");
